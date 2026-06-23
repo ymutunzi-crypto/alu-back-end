ffa7bb63dba7a990ca92ef506e60e361755f7203
ymutunzi-crypto <y.mutunzi@alustudent.com>
2026-06-23 10:44:12 +0000

    Initial commit: Set up repository structure and base files

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This script gathers data from a REST API for a given employee ID
+and returns information about their TODO list progress.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    pass
